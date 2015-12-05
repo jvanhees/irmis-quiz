09f27d3c3cbe7d06ef307e2581498c558b40024e
Jorick van Hees <j.vanhees@bluebillywig.com>
2015-12-05 13:54:27 +0100

    Prod version

--- config/app.js
+++ config/app.js
@@ -1,3 +1,7 @@
 angular.module('quiz', []);
 
 var quiz = angular.module('quiz', ['ui.router']);
+
+quiz.config(['$compileProvider', function ($compileProvider) {
+	$compileProvider.debugInfoEnabled(false);
+}]);
