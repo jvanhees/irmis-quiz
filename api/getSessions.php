4b67719dcaa2da3f840da06071945729f603f9ae
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-01-11 09:53:04 +0100

    Fixes for different servers, other display of data, changed videos

--- api/getSessions.php
+++ api/getSessions.php
@@ -14,19 +14,19 @@ if ($result->num_rows > 0) {
 function getLikert($var){
 	switch($var) {
 		case '0':
-			return '++';
+			return '1';
 		break;
 		case '1':
-			return '+';
+			return '2';
 		break;
 		case '2':
-			return '0';
+			return '3';
 		break;
 		case '3':
-			return '-';
+			return '4';
 		break;
 		case '4':
-			return '--';
+			return '5';
 		break;
 	}
 }
@@ -45,13 +45,16 @@ function evenOdd($number){
 </head>
 <body>
 <h1>Results</h1>
+<p>1: Helemaal oneens, 5: Helemaal eens</p>
 <table>
 	<thead>
 		<tr>
+			<td>#</td>
 			<td>ID</td>
 			<td>Ip</td>
 			<td>Start time</td>
 			<td>End time</td>
+			<td>Duration</td>
 			<td>Questions correct</td>
 			<td>Questions wrong</td>
 			<td>Feedback type</td>
@@ -66,24 +69,33 @@ function evenOdd($number){
 		</tr>
 	</thead>
 	<tbody>
-		<?php foreach($data as $key => $row){ ?>
-			<tr class="<?=evenOdd($key)?>">
-				<td><?=$row['id']?></td>
-				<td><?=$row['ip']?></td>
-				<td><?=$row['starttime']?></td>
-				<td><?=$row['endtime']?></td>
-				<td><?=$row['questions_correct']?></td>
-				<td><?=$row['questions_wrong']?></td>
-				<td><?=$row['feedback_type']?></td>
+		<?php
+		$count = 0;
+		foreach($data as $key => $row){
+			if($row['endtime'] != '0000-00-00 00:00:00' && $row['ip'] != '77.174.136.145'){
+				$count++;
+				$duration = strtotime($row['endtime']) - strtotime($row['starttime']);
+			?>
+				<tr class="<?=evenOdd($count)?>">
+					<td><?=$count?></td>
+					<td><?=$row['id']?></td>
+					<td><?=$row['ip']?></td>
+					<td><?=$row['starttime']?></td>
+					<td><?=$row['endtime']?></td>
+					<td><?=date('i:s', $duration)?></td>
+					<td><?=$row['questions_correct']?></td>
+					<td><?=$row['questions_wrong']?></td>
+					<td><?=$row['feedback_type']?></td>
 				
-				<td><?=getLikert($row['quiz_q1'])?></td>
-				<td><?=getLikert($row['quiz_q2'])?></td>
-				<td><?=getLikert($row['quiz_q3'])?></td>
+					<td><?=getLikert($row['quiz_q1'])?></td>
+					<td><?=getLikert($row['quiz_q2'])?></td>
+					<td><?=getLikert($row['quiz_q3'])?></td>
 				
-				<td><?=getLikert($row['prototype_q1'])?></td>
-				<td><?=getLikert($row['prototype_q2'])?></td>
-				<td><?=getLikert($row['prototype_q3'])?></td>
-			</tr>
+					<td><?=getLikert($row['prototype_q1'])?></td>
+					<td><?=getLikert($row['prototype_q2'])?></td>
+					<td><?=getLikert($row['prototype_q3'])?></td>
+				</tr>
+			<?php } ?>
 		<?php } ?>
 	</tbody>
 </table>
